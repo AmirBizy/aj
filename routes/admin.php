e76b1f852d1e642f0cdb49dc6b6eee28e5767fad
Amirreza Jafari <amiri.pvr@gmail.com>
2025-07-07 23:53:06 +0330

    add account setting route for admin panel

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\AboutMeController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\SettingController;
+use App\Http\Controllers\Admin\AccountController;
 use Illuminate\Support\Facades\Route;
 
 // Admin Routes
@@ -27,6 +28,10 @@ Route::group(
         // setting
         Route::get('/setting', [SettingController::class , 'index'])->name('setting.index');
         Route::patch('/setting/update', [SettingController::class , 'update'])->name('setting.update');
+
+        // setting
+        Route::get('/account', [AccountController::class , 'index'])->name('account.index');
+        Route::patch('/account/update', [AccountController::class , 'update'])->name('account.update');
     }
 );
 // Admin Routes
